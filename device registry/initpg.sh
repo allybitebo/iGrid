b190540b035238131f2c0b91db172e8216e99be3
alphaxadLaravel <alphaxadfundi@gmail.com>
2023-09-25 09:37:13 +0300

    delete files

--- device registry/initpg.sh
+++
@@ -1,9 +0,0 @@
-sudo mkdir /var/lib/postgres/data
-chown postgres /var/lib/postgres/data
-sudo -i -u postgres
-initdb  -D '/var/lib/postgres/data'
-
-
-sudo -iu postgres
-initdb -D /var/lib/postgres/data
-pg_ctl -D /var/lib/postgres/ -l logfile start
--- device registry/region.go
+++
@@ -1,7 +0,0 @@
-package registry
-
-type Region struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Desc string `json:"description"`
-}
